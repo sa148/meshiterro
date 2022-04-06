3adc6de09abc0ecba81f073e6b37f16ea3070b61
sa148 <GitHub>
2022-04-06 10:52:05 +0000

    4-18end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-
   devise_for :users
-  root to: 'homes#top'
-  get "/homeabours/about" => "homes#about" , as: "about"
-  resources :post_images, only: [:new, :index, :show]
-  resources :users, only: [:show, :edit]
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
